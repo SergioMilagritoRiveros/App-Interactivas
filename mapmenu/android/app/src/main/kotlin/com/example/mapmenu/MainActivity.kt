44883c1c6930f2f54a8b71ac5a9dcb77e2fcbdf6
Sergio Riveros <sergioeriveros@gmail.com>
2020-09-30 02:28:09 -0500

    map con falla en api key

---
+++ mapmenu/android/app/src/main/kotlin/com/example/mapmenu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mapmenu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mapmenu/lib/paginas/MapPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MapPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Wrap(children: <Widget>[
+      Container(
+        color: Colors.yellow,
+        width: 500.0,
+        height: 60.0,
+      )
+    ]));
+  }
+}
