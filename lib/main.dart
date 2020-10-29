f3da02721c75b0f9881f9172f6240fcb7a8801a1
UchihaAdri64 <71458406+UchihaAdri64@users.noreply.github.com>
2020-10-29 13:22:53 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:animapp/views/forum.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+import 'views/welcomePage.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,13 +15,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Animapp',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
         textTheme: GoogleFonts.latoTextTheme(textTheme).copyWith(
           bodyText1: GoogleFonts.montserrat(textStyle: textTheme.bodyText1),
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: Forum(title: "Animapp"),
+      home: ResponsiveBuilder(
+        builder: (context, sizing) {
+          return Forum(title: "Animapp");
+        },
+      ),
     );
   }
 }
