c59fb6360148dd6107b6b9690081571dbcdca384
Valeria Rodriguez <valery-rod@hotmail.com>
2020-11-10 22:22:11 -0500

    add basic dark mode to nav drawer

--- lib/widgets/NavDrawer.dart
+++ lib/widgets/NavDrawer.dart
@@ -1,12 +1,16 @@
+import 'package:animapp/blocs/darkThemeProvider.dart';
 import 'package:animapp/views/ScheduledAppointment.dart';
 import 'package:animapp/views/forum.dart';
 import 'package:animapp/views/welcomePage.dart';
 import 'package:animapp/views/whatAreYouSearchingFor.dart';
 import 'package:flutter/material.dart';
+import 'package:lite_rolling_switch/lite_rolling_switch.dart';
+import 'package:provider/provider.dart';
 
 class NavDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final themeChange = Provider.of<DarkThemeProvider>(context);
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -24,33 +28,59 @@ class NavDrawer extends StatelessWidget {
               ),
             ),
           ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 50),
+            child: LiteRollingSwitch(
+              value: !themeChange.darkTheme,
+              textOn: "Ligth",
+              textOff: "Dark",
+              colorOn: Colors.amberAccent[700],
+              colorOff: Colors.grey,
+              iconOn: Icons.wb_sunny,
+              iconOff: Icons.nightlight_round,
+              textSize: 18.0,
+              onChanged: (bool position){
+                themeChange.darkTheme = !position;
+              },
+            ),
+          ),
           ListTile(
             leading: Icon(Icons.search),
             title: Text('Busqueda'),
-            onTap: () => {Navigator.push(context, MaterialPageRoute(
-                        builder: (context) =>
-                            WhatAreYouSearchingFor()))},
+            onTap: () => {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => WhatAreYouSearchingFor()))
+            },
           ),
           ListTile(
             leading: Icon(Icons.chat),
             title: Text('Foros'),
-            onTap: () => {Navigator.push(context, MaterialPageRoute(
-                        builder: (context) =>
-                            Forum(title: "Animapp")))},
+            onTap: () => {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Forum(title: "Animapp")))
+            },
           ),
           ListTile(
             leading: Icon(Icons.settings),
             title: Text('Citas Agendadas'),
-            onTap: () => {Navigator.push(context, MaterialPageRoute(
-                        builder: (context) =>
-                            ScheduledAppointment()))},
+            onTap: () => {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ScheduledAppointment()))
+            },
           ),
           ListTile(
             leading: Icon(Icons.exit_to_app),
             title: Text('Cerrar sesión'),
-            onTap: () => {Navigator.push(context, MaterialPageRoute(
-                        builder: (context) =>
-                            WelcomePage()))},
+            onTap: () => {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => WelcomePage()))
+            },
           ),
         ],
       ),
