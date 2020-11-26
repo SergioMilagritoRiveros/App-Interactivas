b08bd74ff8fb907e4c30d3c004621cc8b32f5463
Valeria Rodriguez <valery-rod@hotmail.com>
2020-11-26 01:03:36 -0500

    add scheduledAppointment detail landscape view

--- lib/views/ScheduledAppoinntment_detail.dart
+++ lib/views/ScheduledAppoinntment_detail.dart
@@ -1,6 +1,7 @@
+import 'package:animapp/global.dart';
 import 'package:animapp/views/ScheduleAppointment_model.dart';
+import 'package:animapp/views/ScheduledAppointment.dart';
 import 'package:flutter/material.dart';
-import '../widgets/InputWidget.dart';
 
 class ScheduledDetail extends StatefulWidget {
   final ScheduleApponitmentModel model;
@@ -30,7 +31,8 @@ class _ScheduledDetailState extends State<ScheduledDetail> {
           if (Orientation.portrait == orientation) {
             return _scheduledDetailPortraitBody(size, context);
           } else {
-            return _scheduledDetailLandscapeBody(size, context);
+            Navigator.pop(context);
+            return Container();
           }
         },
       ),
@@ -38,56 +40,67 @@ class _ScheduledDetailState extends State<ScheduledDetail> {
   }
 
   Widget _scheduledDetailWidget(Size size) {
-    return ListView(
-      shrinkWrap: true,
-      children: [
-        SizedBox(height: size.height / 8),
-        Text(
-          '${widget.model.type}',
-          style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-          textAlign: TextAlign.center,
-        ),
-        Container(
-          margin: EdgeInsets.only(top: size.height / 30),
-          height: size.height / 4,
-          child: Image.asset(widget.model.imageURL),
-        ),
-        SizedBox(height: size.height / 40),
-        Text(
-          '${widget.model.name}',
-          style: TextStyle(fontSize: 20),
-        ),
-        SizedBox(height: size.height / 40),
-        Text('Dirección: ${widget.model.address}',
-            style: TextStyle(fontSize: 15)),
-        SizedBox(height: size.height / 40),
-        Text('Fecha/Hora: ${widget.model.date}'),
-        SizedBox(height: size.height / 40),
-        RaisedButton(
-          color: Colors.redAccent[400],
-          onPressed: () {
-            var snackBar = SnackBar(
-              content: Text('Se ha cancelado tu cita.'),
-              action: SnackBarAction(
-                label: 'Entendido',
-                onPressed: () => Navigator.pop(context),
-              ),
-            );
-            Scaffold.of(context).showSnackBar(snackBar);
-          },
-          child: Container(
-            padding: EdgeInsets.symmetric(vertical: size.height / 50),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Cancelar',
-                    style: TextStyle(color: Colors.white),
-                    textAlign: TextAlign.center),
-              ],
-            ),
+    return Center(
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          SizedBox(height: size.height / 40),
+          Text(
+            '${widget.model.type}',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
           ),
-        )
-      ],
+          Container(
+            margin: EdgeInsets.only(top: size.height / 30),
+            height: size.height / 4,
+            child: Image.asset(widget.model.imageURL),
+          ),
+          SizedBox(height: size.height / 40),
+          Text(
+            '${widget.model.name}',
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: size.height / 40),
+          Text(
+            'Dirección: ${widget.model.address}',
+            style: TextStyle(fontSize: 15),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: size.height / 40),
+          Text(
+            'Fecha/Hora: ${widget.model.date}',
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: size.height / 40),
+          Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(right: size.width / 40),
+              child: RaisedButton(
+                textColor: Colors.white,
+                color: Colors.redAccent[400],
+                onPressed: () {
+                  var snackBar = SnackBar(
+                    content: Text('Se ha cancelado su cita.'),
+                    action: SnackBarAction(
+                      label: 'Entendido',
+                      onPressed: () {},
+                    ),
+                  );
+                  Scaffold.of(context).showSnackBar(snackBar);
+                },
+                child: Container(
+                  child: new Text("Cancelar", textAlign: TextAlign.center),
+                  width: MediaQuery.of(context).size.width / 7,
+                ),
+              ),
+            ),
+          ],
+        ),
+        ],
+      ),
     );
   }
 
@@ -143,67 +156,6 @@ class _ScheduledDetailState extends State<ScheduledDetail> {
           )
         ],
       ),
-    );
-  }
-
-  Widget _scheduledDetailLandscapeBody(size, context) {
-    return Row(
-      children: [
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: size.width / 30),
-          width: size.width / 3,
-          child: ListView(
-            children: [
-              SizedBox(height: size.height / 8),
-              Text(
-                '${widget.model.type}',
-                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-              Container(
-                margin: EdgeInsets.only(top: size.height / 30),
-                height: size.height / 4,
-                child: Image.asset(widget.model.imageURL),
-              ),
-              SizedBox(height: size.height / 40),
-              Text(
-                '${widget.model.name}',
-                style: TextStyle(fontSize: 20),
-              ),
-              SizedBox(height: size.height / 40),
-              Text('Dirección: ${widget.model.address}',
-                  style: TextStyle(fontSize: 15)),
-              SizedBox(height: size.height / 40),
-              Text('Fecha/Hora: ${widget.model.date}'),
-              SizedBox(height: size.height / 40),
-              RaisedButton(
-                color: Colors.redAccent[400],
-                onPressed: () {
-                  var snackBar = SnackBar(
-                    content: Text('Se ha cancelado tu cita.'),
-                    action: SnackBarAction(
-                      label: 'Entendido',
-                      onPressed: () => Navigator.pop(context),
-                    ),
-                  );
-                  Scaffold.of(context).showSnackBar(snackBar);
-                },
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: size.height / 50),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text('Cancelar',
-                          style: TextStyle(color: Colors.white),
-                          textAlign: TextAlign.center),
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ],
     );
   }
 }
