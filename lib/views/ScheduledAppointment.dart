a2218d467b2f6176e0ab2b5700cf38cad633548c
Valeria Rodriguez <valery-rod@hotmail.com>
2020-11-25 22:50:41 -0500

    refactor code

--- lib/views/ScheduledAppointment.dart
+++ lib/views/ScheduledAppointment.dart
@@ -1,3 +1,5 @@
+import 'package:animapp/views/ScheduleAppointment_model.dart';
+import 'package:animapp/views/ScheduledAppoinntment_detail.dart';
 import 'package:animapp/widgets/NavDrawer.dart';
 import 'package:animapp/widgets/showMenu.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +12,7 @@ class ScheduledAppointment extends StatefulWidget {
 }
 
 class _ScheduledAppointmentState extends State<ScheduledAppointment> {
+  ScheduleApponitmentModel _selected;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,352 +33,148 @@ class _ScheduledAppointmentState extends State<ScheduledAppointment> {
           if (orientation == Orientation.portrait) {
             return _buildPortrait(size);
           } else {
-            return _buildLandscape(size);
+            return _buildPortrait(size);
           }
         },
       ),
     );
   }
 
+Widget _buildSelected(Size size) {
+    if (_selected != null) {
+      return Container(
+        padding: EdgeInsets.only(left: size.width / 50),
+        width: 2 * size.width / 3,
+        child: ForumDetail(
+          title: widget.title,
+          forum: _selected,
+          isWidget: true,
+        ),
+      );
+    }
+    return Container();
+  }
+
   Widget _buildLandscape(Size size) {
-    return ListView(
-      scrollDirection: Axis.vertical,
-      children: <Widget>[
-        SizedBox(height: size.height / 50),
-        Text(
-          "Tus Citas Agendadas",
-          style: TextStyle(fontSize: 30.0),
-          textAlign: TextAlign.center,
-        ),
+    ScheduleApponitmentModel _selected;
+    return Row(
+      children: [
         Container(
-          margin: EdgeInsets.symmetric(vertical: size.height / 50),
-          height: size.height / 4,
-          child: new FittedBox(
-            child: Material(
-              color: Colors.transparent,
-              elevation: 14.0,
-              borderRadius: BorderRadius.circular(24.0),
-              shadowColor: Color(0x802196F3),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    child: Padding(
-                      padding: EdgeInsets.only(left: size.width / 50),
-                      child: myDetailsContainer1(),
-                    ),
-                  ),
-                  Container(
-                    height: size.height / 3,
-                    child: ClipRRect(
-                      borderRadius: new BorderRadius.circular(24.0),
-                      child: Image(
-                        fit: BoxFit.contain,
-                        alignment: Alignment.topRight,
-                        image: AssetImage('images/image-card.jpg'),
-                      ),
-                    ),
-                  ),
-                ],
+          padding: EdgeInsets.only(left: size.width / 50),
+          width: size.width / 3,
+          child: Column(
+            children: [
+              SizedBox(height: size.height / 50),
+              Text(
+                "Tus Citas Agendadas",
+                style: TextStyle(fontSize: 30.0),
+                textAlign: TextAlign.center,
               ),
-            ),
+              Expanded(
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: scheduleAppointmentList.length,
+                    itemBuilder: (context, index) {
+                      var list = scheduleAppointmentList[index];
+                      return RaisedButton(
+                        elevation: 0,
+                        color: Colors.transparent,
+                        onPressed: () => setState(() => _selected = list),
+                        child: Card(
+                          child: Column(
+                            children: [
+                              Card(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(24.0)),
+                                elevation: 14.0,
+                                shadowColor: Color(0x802196F3),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Container(
+                                      child: Text("${list.type}"),
+                                    ),
+                                    Container(
+                                      height: size.height / 5,
+                                      child: ClipRRect(
+                                        borderRadius:
+                                            new BorderRadius.circular(24.0),
+                                        child: Image(
+                                          fit: BoxFit.contain,
+                                          alignment: Alignment.topRight,
+                                          image: AssetImage(
+                                              'images/image-card.png'),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          elevation: 5.0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                        ),
+                      );
+                    }),
+              )
+            ],
           ),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(vertical: size.height / 50),
-          height: size.height / 4,
-          child: new FittedBox(
-            child: Material(
-              color: Colors.white,
-              elevation: 14.0,
-              borderRadius: BorderRadius.circular(24.0),
-              shadowColor: Color(0x802196F3),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    child: Padding(
-                      padding: EdgeInsets.only(left: size.width / 50),
-                      child: myDetailsContainer2(),
-                    ),
-                  ),
-                  Container(
-                    height: size.height / 3,
-                    child: ClipRRect(
-                      borderRadius: new BorderRadius.circular(24.0),
-                      child: Image(
-                        fit: BoxFit.contain,
-                        alignment: Alignment.topRight,
-                        image: AssetImage('images/image-card.jpg'),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        Container(
-          height: size.height / 4,
-          margin: EdgeInsets.symmetric(vertical: size.height / 50),
-          child: new FittedBox(
-            child: Material(
-              color: Colors.white,
-              elevation: 14.0,
-              borderRadius: BorderRadius.circular(24.0),
-              shadowColor: Color(0x802196F3),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    child: Padding(
-                      padding: EdgeInsets.only(left: size.width / 50),
-                      child: myDetailsContainer3(),
-                    ),
-                  ),
-                  Container(
-                    height: size.height / 3,
-                    child: ClipRRect(
-                      borderRadius: new BorderRadius.circular(24.0),
-                      child: Image(
-                        fit: BoxFit.contain,
-                        alignment: Alignment.topRight,
-                        image: AssetImage('images/image-card.jpg'),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
+        )
       ],
     );
   }
 
   Widget _buildPortrait(Size size) {
-    Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24.0)),
-      elevation: 14.0,
-      shadowColor: Color(0x802196F3),
-    );
-    return ListView(
-      scrollDirection: Axis.vertical,
-      children: <Widget>[
-        SizedBox(height: size.height / 50),
-        Text(
-          "Tus Citas Agendadas",
-          style: TextStyle(fontSize: 30.0),
-          textAlign: TextAlign.center,
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(
-              horizontal: size.width / 45, vertical: size.height / 50),
-          child: new FittedBox(
-            child: Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24.0)),
-              elevation: 14.0,
-              shadowColor: Color(0x802196F3),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    child: Padding(
-                      padding: EdgeInsets.only(left: size.width / 50),
-                      child: myDetailsContainer1(),
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: size.width / 20),
+      child: Column(
+        children: [
+          SizedBox(height: size.height / 25),
+          Text(
+            'Tus citas agendadas',
+            style: TextStyle(fontSize: 30.0),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: size.height / 25),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: scheduleAppointmentList.length,
+              itemBuilder: (context, index) {
+                var post = scheduleAppointmentList[index];
+                return RaisedButton(
+                  elevation: 0,
+                  color: Colors.transparent,
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          ScheduledDetail(model: post)
                     ),
                   ),
-                  Container(
-                    height: size.height / 5,
-                    child: ClipRRect(
-                      borderRadius: new BorderRadius.circular(24.0),
-                      child: Image(
-                        fit: BoxFit.contain,
-                        alignment: Alignment.topRight,
-                        image: AssetImage('images/image-card.png'),
-                      ),
+                  child: Card(
+                    child: Column(
+                      children: [
+                        Image.asset(post.imageURL, width: size.width/4),
+                        ListTile(
+                          title: Text(post.type, textAlign: TextAlign.center),
+                          subtitle: Text("${post.name}",  textAlign: TextAlign.center),
+                        ),
+                      ],
                     ),
+                    elevation: 5.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
                   ),
-                ],
-              ),
+                );
+              },
             ),
           ),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(
-              horizontal: size.width / 45, vertical: size.height / 50),
-          child: new FittedBox(
-            child: Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24.0)),
-              elevation: 14.0,
-              shadowColor: Color(0x802196F3),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    child: Padding(
-                      padding: EdgeInsets.only(left: size.width / 50),
-                      child: myDetailsContainer2(),
-                    ),
-                  ),
-                  Container(
-                    height: size.height / 5,
-                    child: ClipRRect(
-                      borderRadius: new BorderRadius.circular(24.0),
-                      child: Image(
-                        fit: BoxFit.contain,
-                        alignment: Alignment.topRight,
-                        image: AssetImage('images/image-card.png'),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(
-              horizontal: size.width / 45, vertical: size.height / 50),
-          child: new FittedBox(
-            child: Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24.0)),
-              elevation: 14.0,
-              shadowColor: Color(0x802196F3),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    child: Padding(
-                      padding: EdgeInsets.only(left: size.width / 50),
-                      child: myDetailsContainer3(),
-                    ),
-                  ),
-                  Container(
-                    height: size.height / 5,
-                    child: ClipRRect(
-                      borderRadius: new BorderRadius.circular(24.0),
-                      child: Image(
-                        fit: BoxFit.contain,
-                        alignment: Alignment.topRight,
-                        image: AssetImage('images/image-card.png'),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget myDetailsContainer1() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(
-              child: Text(
-            "Baño",
-            style: TextStyle(
-                fontSize: 30.0,
-                color: Colors.amber[600],
-                fontWeight: FontWeight.bold),
-          )),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(
-              child: Text(
-            "Veterinaria Dog Pet \u00B7 20/09/2020  11:40 am",
-            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-          )),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(
-              child: Text(
-            "Cra. 78 #4-06, Bogotá, Cundinamarca",
-            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-          )),
-        )
-      ],
-    );
-  }
-
-  Widget myDetailsContainer2() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(
-              child: Text(
-            "Consulta Veterinario",
-            style: TextStyle(
-                color: Colors.amber[600],
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold),
-          )),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(
-              child: Text(
-            "Veterinaria perro feliz \u00B7 23/09/2020  9:00 am",
-            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-          )),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(
-              child: Text(
-            "Ciudad De Cali #36-21, Kennedy, Bogotá",
-            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-          )),
-        )
-      ],
-    );
-  }
-
-  Widget myDetailsContainer3() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(
-              child: Text(
-            "Consulta Veterinario",
-            style: TextStyle(
-                color: Colors.amber[600],
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold),
-          )),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(
-              child: Text(
-            "Veterinaria Doblesse \u00B7 30/09/2020  01:30 pm",
-            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-          )),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: Container(
-              child: Text(
-            " Cra. 21 #1, Antonio Nariño, Bogotá",
-            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-          )),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
